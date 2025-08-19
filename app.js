9972d4825c718ecb144f7f34d8136cdb3ca590b3
ananthakrishn04 <sananthakrishn04@gmail.com>
2025-08-19 20:17:16 +0530

    add entrypoint app + package.json

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 import express from "express"
 import mongoose from "mongoose"
 import dotenv from "dotenv"
+import UserRoutes from "./routes/user.routes.js"
+import ProductRoutes from "./routes/product.routes.js"
+import CartRoutes from "./routes/cart.routes.js"
 
 dotenv.config()
 
@@ -9,23 +12,17 @@ const PORT = process.env.PORT || 5050
 const app = express()
 app.use(express.json())
 
-mongoose.connect("mongodb://localhost:27017/").then(() => {
+mongoose.connect(process.env.MONGO_URI).then(() => {
     console.info("DB CONNECTION SUCCESSFULL")
 }).catch(() => {
     console.info("DB CONNECTION FAILED")
 })
 
-
-
-
+app.use("/api/user", UserRoutes)
+app.use("/api/products", ProductRoutes)
+app.use("/api/cart", CartRoutes)
 
 
 app.listen(PORT , () => {
     console.info(`SERVER LISTENING AT PORT : ${PORT}`)
 })
-
-
-
-
-
-
